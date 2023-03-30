d478d86ec4add120edd9085dc0d196eeda8f5320
hungry samurai <kryjeka@mail.ru>
2023-03-30 10:39:01 +0300

    refctor

--- pic_comparer/comparer.js
+++ pic_comparer/comparer.js
@@ -1,51 +1,12 @@
 class Comparer {
+
+  #id = Date.now() + Math.floor(Math.random() * 1000);
+
   constructor(parentContainer, { enableUpload, enableDragDrop, bgLink, fgLink }) {
-    this.id = Math.floor(Math.random() * 10000);
     this.parentContainer = parentContainer;
 
-    // Init DOM elements
+    // Init DOM elements based on settings
     this.initDOMElements(enableUpload, enableDragDrop);
-
-    this.comparerContainer = this.parentContainer.querySelector(".comparer-container");
-    this.range = this.parentContainer.querySelector(`#comparer-range-${this.id}`);
-    this.fgImage = this.parentContainer.querySelector(".comparer-fg");
-    this.bgImage = this.parentContainer.querySelector(".comparer-bg");
-    this.handler = this.parentContainer.querySelector(".comparer-handler");
-
-    // Init upload buttons
-    if (enableUpload) {
-      this.imageInput1 = this.parentContainer.querySelector(`#comparer-upload-${this.id}`);
-      this.imageInput2 = this.parentContainer.querySelector(`#comparer-upload-${this.id}-2`);
-      this.uploadBtn1 = this.parentContainer.querySelector(`#comparer-upload-btn-1-${this.id}`);
-      this.uploadBtn2 = this.parentContainer.querySelector(`#comparer-upload-btn-2-${this.id}`);
-    }
-
-    // Init Drag'n'Drop Area
-    if (enableDragDrop) {
-      this.dropArea = this.parentContainer.querySelector('.comparer-drop-area');
-      this.dropInner = this.parentContainer.querySelector('.comparer-drop-inner');
-
-      // Prevent defaults for drag events
-      ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-        this.dropArea.addEventListener(eventName, this.preventDefaults);
-      });
-
-      // Highlight/unhighlight area
-      ['dragenter', 'dragover'].forEach(eventName => {
-        this.dropArea.addEventListener(eventName, () => {
-          this.highlight(this.dropArea)
-        });
-      });
-
-      ['dragleave', 'drop'].forEach(eventName => {
-        this.dropArea.addEventListener(eventName, () => {
-          this.dropArea.addEventListener(eventName, () => {
-            this.unhighlight(this.dropArea)
-          });
-        });
-      });
-
-    }
 
     // Init state object
     this.state = {
@@ -64,10 +25,9 @@ class Comparer {
           foreground: true
         }
 
-        this.classChanger(this.uploadBtn1, 'empty', 'full');
-        this.classChanger(this.uploadBtn2, 'empty', 'full');
+        this.switchClass(this.uploadBtnBG, 'empty', 'full');
+        this.switchClass(this.uploadBtnFG, 'empty', 'full');
       }
-
     }
 
     // Add events
@@ -75,66 +35,173 @@ class Comparer {
   }
 
   initDOMElements(uploadArea, dropArea) {
-    const comparerContainer = document.createElement('div');
-    comparerContainer.classList.add('comparer-container');
-    comparerContainer.innerHTML = `
-            <div class="comparer-bg"></div>
-            <div class="comparer-fg"></div>
+
+    // Init Comparer  container
+    this.comparerContainer = document.createElement('div');
+    this.comparerContainer.classList.add('comparer-container');
+    this.comparerContainer.innerHTML = `
+        <div class="comparer-bg"></div>
+        <div class="comparer-fg"></div>
 
         <input
           type="range"
           step="0.01"
           value="50"
-          id="comparer-range-${this.id}"
+          id="comparer-range-${this.#id}"
           class="comparer-range"
           max="100"
           min="0"
         />
+
         <div class="comparer-handler"></div>
     `;
-    this.parentContainer.append(comparerContainer);
 
-    // Add upload Buttons
+    this.range = this.comparerContainer.querySelector(`#comparer-range-${this.#id}`);
+    this.fgImage = this.comparerContainer.querySelector(".comparer-fg");
+    this.bgImage = this.comparerContainer.querySelector(".comparer-bg");
+    this.handler = this.comparerContainer.querySelector(".comparer-handler");
+
+    this.parentContainer.append(this.comparerContainer);
+
+    // Init upload Buttons
     if (uploadArea) {
       const uploadContainer = document.createElement('div');
       uploadContainer.classList.add('comparer-upload-container');
       uploadContainer.innerHTML = `
-            <input
+        
+        <input
           type="file"
-          id="comparer-upload-${this.id}"
+          id="comparer-upload-${this.#id}-foreground"
           accept="image/jpeg, image/png, image/jpg"
           hidden
         />
         <input
           type="file"
-          id="comparer-upload-${this.id}-2"
+          id="comparer-upload-${this.#id}-background"
           accept="image/jpeg, image/png, image/jpg"
           hidden
         />
+
         <label
-          for="comparer-upload-${this.id}-2"
-          id="comparer-upload-btn-2-${this.id}"
+          for="comparer-upload-${this.#id}-foreground"
+          id="comparer-upload-btn-foreground-${this.#id}"
           class="comparer-btn empty"
           >Background</label
         >
         <label
-          for="comparer-upload-${this.id}"
-          id="comparer-upload-btn-1-${this.id}"
+          for="comparer-upload-${this.#id}-background"
+          id="comparer-upload-btn-background-${this.#id}"
           class="comparer-btn empty"
           >Foreground</label
         >
     `;
+
+      this.imageInputBackground = uploadContainer.querySelector(`#comparer-upload-${this.#id}-background`);
+      this.imageInputForeground = uploadContainer.querySelector(`#comparer-upload-${this.#id}-foreground`);
+      this.uploadBtnBG = uploadContainer.querySelector(`#comparer-upload-btn-background-${this.#id}`);
+      this.uploadBtnFG = uploadContainer.querySelector(`#comparer-upload-btn-foreground-${this.#id}`);
+
       this.parentContainer.append(uploadContainer);
     }
 
     // Add Drag'n'Drop area
     if (dropArea) {
-      const dropContainer = document.createElement('div');
-      dropContainer.classList.add('comparer-drop-area');
-      dropContainer.innerHTML = `
-      <div class="comparer-drop-inner">Drop some images to compare</div>
-`;
-      this.parentContainer.append(dropContainer);
+      this.dropArea = document.createElement('div');
+      this.dropArea.classList.add('comparer-drop-area');
+
+      this.dropInner = document.createElement('div');
+      this.dropInner.classList.add('comparer-drop-inner');
+      this.dropInner.textContent = 'Drop some images to compare';
+
+      this.dropArea.append(this.dropInner)
+
+      this.parentContainer.append(this.dropArea);
+
+      // Prevent defaults for drag events
+
+      ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        this.dropArea.addEventListener(eventName, this.preventDefaults);
+      });
+
+      // Highlight/unhighlight area
+
+      ['dragenter', 'dragover'].forEach(eventName => {
+        this.dropArea.addEventListener(eventName, () => {
+          this.highlight(this.dropArea)
+        });
+      });
+
+      ['dragleave', 'drop'].forEach(eventName => {
+        this.dropArea.addEventListener(eventName, () => {
+          this.dropArea.addEventListener(eventName, () => {
+            this.unhighlight(this.dropArea)
+          });
+        });
+      });
+    }
+  }
+
+  addEvents(buttons, dragDrop) {
+    // Drag-to-compare range
+    this.range.addEventListener("input", (e) => {
+      const position = e.target.value;
+      const px = Math.floor((parseInt(getComputedStyle(this.comparerContainer).width)) / 100 * position);
+      this.handler.style.left = `${position}%`;
+      this.fgImage.style.width = `${px}px`;
+    });
+
+    this.range.addEventListener('mouseenter', () => {
+      this.handler.classList.add('active')
+    });
+
+    this.range.addEventListener('mouseleave', () => {
+      this.handler.classList.remove('active')
+    });
+
+    this.range.addEventListener('touchstart', () => {
+      this.handler.classList.add('active')
+    })
+
+    this.range.addEventListener('touchend', () => {
+      this.handler.classList.remove('active')
+    })
+
+    if (buttons) {
+      // Foreground image upload via button
+      this.imageInputBackground.addEventListener("input", (e) => {
+        // Check file type
+        if (!this.checkFile(e.target.files[0])) return;
+        this.switchClass(this.uploadBtnBG, 'empty', 'full');
+        this.generatePreview(this.fgImage, e.target.files[0]);
+        this.state.foreground = true;
+      });
+
+      // Background image upload via button
+      this.imageInputForeground.addEventListener("input", (e) => {
+        // Check file type
+        if (!this.checkFile(e.target.files[0])) return;
+        this.switchClass(this.uploadBtnFG, 'empty', 'full');
+        this.generatePreview(this.bgImage, e.target.files[0], true);
+        this.state.background = true;
+      });
+    }
+    // Drag'n'Drop area uploads
+    if (dragDrop) {
+      this.dropArea.addEventListener('drop', (e) => {
+        let dt = e.dataTransfer
+        let files = dt.files
+
+        if (files.length > 2) {
+          this.dropInner.textContent = 'Please drop one or two images!';
+          return;
+        }
+        if ([...files].map(file => {
+          if (!this.checkFile(file)) return false;
+          return true;
+        }).includes(false)) return;
+
+        this.handleFiles(files)
+      })
     }
   }
 
@@ -159,8 +226,7 @@ class Comparer {
     }
   }
 
-
-  classChanger(el, class1, class2) {
+  switchClass(el, class1, class2) {
     el.classList.remove(class1);
     el.classList.add(class2)
   }
@@ -190,19 +256,19 @@ class Comparer {
           this.generatePreview(this.bgImage, file, true);
         }
       });
-      this.classChanger(this.uploadBtn1, 'empty', 'full');
-      this.classChanger(this.uploadBtn2, 'empty', 'full');
+      this.switchClass(this.uploadBtnBG, 'empty', 'full');
+      this.switchClass(this.uploadBtnFG, 'empty', 'full');
       this.state.foreground = true;
       this.state.background = true;
     }
     else if (files.length === 1) {
       if (!this.state.background && !this.state.foreground) {
         this.generatePreview(this.bgImage, files[0], true);
-        this.classChanger(this.uploadBtn2, 'empty', 'full');
+        this.switchClass(this.uploadBtnFG, 'empty', 'full');
         this.state.background = true;
       } else if (this.state.background && !this.state.foreground) {
         this.generatePreview(this.fgImage, files[0]);
-        this.classChanger(this.uploadBtn1, 'empty', 'full');
+        this.switchClass(this.uploadBtnBG, 'empty', 'full');
         this.state.foreground = true;
       } else if (this.state.background && this.state.foreground) {
         this.generatePreview(this.bgImage, files[0], true);
@@ -216,72 +282,6 @@ class Comparer {
     if (!file) return false;
     if (!file.type.startsWith('image/')) return false;
     return true;
-  }
-
-  addEvents(buttons, dragDrop) {
-    // Drag-to-compare range
-    this.range.addEventListener("input", (e) => {
-      const position = e.target.value;
-      const px = Math.floor((parseInt(getComputedStyle(this.comparerContainer).width)) / 100 * position);
-      this.handler.style.left = `${position}%`;
-      this.fgImage.style.width = `${px}px`;
-      // this.state.background = true;
-    });
-
-    this.range.addEventListener('mouseenter', () => {
-      this.handler.classList.add('active')
-    });
-
-    this.range.addEventListener('mouseleave', () => {
-      this.handler.classList.remove('active')
-    });
-
-    this.range.addEventListener('touchstart', () => {
-      this.handler.classList.add('active')
-    })
-
-    this.range.addEventListener('touchend', () => {
-      this.handler.classList.remove('active')
-    })
-
-    if (buttons) {
-      // Foreground image upload via button
-      this.imageInput1.addEventListener("change", (e) => {
-        // Check file type
-        if (!this.checkFile(e.target.files[0])) return;
-        this.classChanger(this.uploadBtn1, 'empty', 'full');
-        this.generatePreview(this.fgImage, e.target.files[0]);
-        this.state.foreground = true;
-      });
-
-      // Background image upload via button
-      this.imageInput2.addEventListener("change", (e) => {
-        // Check file type
-        if (!this.checkFile(e.target.files[0])) return;
-        this.classChanger(this.uploadBtn2, 'empty', 'full');
-        this.generatePreview(this.bgImage, e.target.files[0], true);
-        this.state.background = true;
-      });
-    }
-    // Drag'n'Drop area uploads
-    if (dragDrop) {
-      this.dropArea.addEventListener('drop', (e) => {
-        let dt = e.dataTransfer
-        let files = dt.files
-
-        if (files.length > 2) {
-          this.dropInner.textContent = 'Please drop one or two images!';
-          return;
-        }
-        if ([...files].map(file => {
-          if (!this.checkFile(file)) return false;
-          return true;
-        }).includes(false)) return;
-
-        this.handleFiles(files)
-      })
-    }
-
   }
 }
 
